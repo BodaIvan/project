52ca3972b75bb6808203d644e768601f378c3c08
BodaIvan <43072796+BodaIvan@users.noreply.github.com>
2020-02-27 17:54:57 +0200

    Add files via upload

---
+++ task1.rb
@@ -0,0 +1,32 @@
+puts "he" #write msg in console 
+int=4 #integer type
+fl=3.12 #float type
+str="ruby" 
+puts ("#{int}, #{fl}, #{str}")
+#4int=5 wrong name
+a=1
+b=2
+total= a + 
+b
+a=9
+b=3
+c=6
+d=3
+result1=a/b
+result2=c/d
+puts "#{result1} #{result2}"
+result3=fl/0
+puts result3
+result4=int%fl
+puts result4
+pow=b**3
+puts pow
+str1='ruby1\'fdsg\''
+puts str1
+str2="sdgawegh"
+sum=str1+str2
+puts sum
+puts (sum=str1*5)
+str3 = "I love Ruby"
+puts str3[7]
+
